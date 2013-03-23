efd3d575dc7ea05e2ed0a0e0d37fd186928ce6e1
ande3577 <ande3577@gmail.com>
2013-03-23 18:40:39 -0500

    Intial Commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# redmine_process_workflow
+
+Works on redmine 2.3.x
+
+
---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_process_workflow do
+  name 'Redmine Process Workflow plugin'
+  author 'David S Anderson'
+  description 'Add a process based workflow to redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
