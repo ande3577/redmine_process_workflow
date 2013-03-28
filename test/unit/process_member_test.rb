4908e1712d761629fd364c8cf450702fc1602b5c
ande3577 <ande3577@gmail.com>
2013-03-27 22:29:13 -0500

    Implement process member

--- test/unit/process_member_test.rb
+++ test/unit/process_member_test.rb
@@ -1,9 +1,29 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class ProcessMemberTest < ActiveSupport::TestCase
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :users
+  fixtures :trackers
+  
+  def setup
+    @user = User.first
+    @process_role = ProcessRole.new(:tracker => Tracker.first, :name => 'role_name')
+  end
+  
+  def test_create
+    member = ProcessMember.new(:process_role => @process_role, :user => @user)
+    assert member.save
+    
+    assert_equal @process_role, member.process_role
+    assert_equal @user, member.user
+  end
+  
+  def test_create_without_process_role
+    member = ProcessMember.new(:user => @user)
+    assert !member.save
+  end
+  
+  def test_create_without_user
+    member = ProcessMember.new(:process_role => @process_role)
+    assert !member.save
   end
 end
