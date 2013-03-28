b6fe48056425beb307ca46018997fa484dd490d0
ande3577 <ande3577@gmail.com>
2013-03-27 19:59:45 -0500

    Test that process step variables updated

--- test/unit/process_step_test.rb
+++ test/unit/process_step_test.rb
@@ -1,9 +1,40 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class ProcessStepTest < ActiveSupport::TestCase
+  fixtures :trackers
+  fixtures :issue_statuses
+  
+  def setup
+    @tracker = Tracker.first
+    @status = IssueStatus.first
+  end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_create
+    step = ProcessStep.new(:name => 'name', :issue_status => @status, :tracker => @tracker)
+    assert step.save
+    
+    assert_equal 'name', step.name
+    assert_equal @status, step.issue_status
+    assert_equal @tracker, step.tracker
+  end
+  
+  def test_create_without_tracker
+    step = ProcessStep.new(:name => 'name', :issue_status => @status)
+    assert !step.save
+  end
+  
+  def test_create_without_status
+    step = ProcessStep.new(:name => 'name', :tracker => @tracker)
+    assert !step.save
+  end
+  
+  def test_create_without_name
+    step = ProcessStep.new(:issue_status => @status, :tracker => @tracker)
+    assert !step.save
+  end
+  
+  def test_create_empty_name
+    step = ProcessStep.new(:name => '', :issue_status => @status, :tracker => @tracker)
+    assert !step.save
   end
 end
