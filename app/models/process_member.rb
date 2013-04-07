b4ba5b968c52ccd154d0ca813a552f455610e715
ande3577 <ande3577@gmail.com>
2013-04-07 00:13:17 -0500

    Update issue assigned to when saving process_member

--- app/models/process_member.rb
+++ app/models/process_member.rb
@@ -1,10 +1,20 @@
 class ProcessMember < ActiveRecord::Base
+  include Redmine::SafeAttributes
   unloadable
   
   belongs_to :user
   belongs_to :process_role
   belongs_to :issue
   
+  after_save :update_issue_assigned_to
+  
   validates_presence_of :user, :process_role, :issue
+  
+  def update_issue_assigned_to
+    if user_id_changed? && !issue.process_step.nil? && (issue.process_step.process_role == process_role)
+      issue.assigned_to = user
+      issue.save
+    end
+  end
   
 end
