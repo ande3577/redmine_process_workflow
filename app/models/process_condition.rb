d5516e1b5b1176f95257595311f8bce8e1bb0669
ande3577 <ande3577@gmail.com>
2013-04-06 19:31:22 -0500

    Next step has separate next state if true, false

--- app/models/process_condition.rb
+++ app/models/process_condition.rb
@@ -1,22 +1,30 @@
+class NextStepValidator < ActiveModel::Validator
+  def validate(record)
+    if record.step_if_true.nil? && record.step_if_false.nil?
+      record.errors[:base] << "Must specify at least one next step."
+    end
+  end
+end
+
 class ProcessCondition < ActiveRecord::Base
   unloadable
   
   belongs_to :process_field
-  belongs_to :process_step
   
-  validates_presence_of :process_step, :process_field
-  validates :comparison_mode, :inclusion => { :in => %w(eql? lt? gt? ne?) }
+  has_one :step_if_true, :class_name => 'ProcessStep', :foreign_key => 'step_if_true_id'
+    has_one :step_if_false, :class_name => 'ProcessStep', :foreign_key => 'step_if_false_id'
   
+  validates_with NextStepValidator
+  validates_presence_of :process_field
+  validates :comparison_mode, :inclusion => { :in => %w(eql? ne?) }
+
   def evaluate(value)
     case comparison_mode
     when 'eql?'
       return value.eql?(field_value)
-    when 'lt?'
-      return value < field_value
-    when 'gt?'
-      return value > field_value
     when 'ne?'
-      return value.ne?(field_value)
+      return !value.eql?(field_value)
     end
   end
 end
+
--- test/unit/process_condition_test.rb
+++ test/unit/process_condition_test.rb
@@ -18,32 +18,39 @@ class ProcessConditionTest < ActiveSupport::TestCase
   end
   
   def test_create
-    condition = ProcessCondition.new(:process_field => @field, :field_value => 'value', :process_step => @step, :comparison_mode => 'eql?')
+    condition = ProcessCondition.new(:process_field => @field, :field_value => 'value', :step_if_true => @step, :comparison_mode => 'eql?')
     assert condition.save
     
     assert_equal @field, condition.process_field
     assert_equal 'value', condition.field_value
-    assert_equal @step, condition.process_step
+    assert_equal @step, condition.step_if_true
     assert_equal 'eql?', condition.comparison_mode
   end
   
   def test_create_without_field
-    condition = ProcessCondition.new(:field_value => 'value', :process_step => @step, :comparison_mode => 'eql?')
+    condition = ProcessCondition.new(:field_value => 'value', :step_if_true => @step, :comparison_mode => 'eql?')
     assert !condition.save
   end
   
   def test_create_without_field_value
-    condition = ProcessCondition.new(:process_field => @field, :process_step => @step, :comparison_mode => 'eql?')
+    condition = ProcessCondition.new(:process_field => @field, :step_if_true => @step, :comparison_mode => 'eql?')
     assert condition.save, "allow creating with blank value"
   end
   
-  def test_create_without_step
-    condition = ProcessCondition.new(:field_value => 'value', :process_step => @step, :comparison_mode => 'eql?')
+  def test_create_without_next_steps
+    condition = ProcessCondition.new(:process_field => @field, :field_value => 'value', :comparison_mode => 'eql?')
     assert !condition.save
   end
   
+  def test_create_with_next_step_if_false
+    condition = ProcessCondition.new(:process_field => @field, :field_value => 'value', :step_if_false => @step, :comparison_mode => 'eql?')
+    assert condition.save
+    
+    assert_equal @step, condition.step_if_false
+  end
+  
   def test_comparison_mode
-    condition = ProcessCondition.new(:process_field => @field, :field_value => 'value', :process_step => @step)
+    condition = ProcessCondition.new(:process_field => @field, :field_value => 'value', :step_if_true => @step)
     assert !condition.save
     
     condition.comparison_mode = 'invalid'
@@ -52,33 +59,25 @@ class ProcessConditionTest < ActiveSupport::TestCase
     condition.comparison_mode = 'eql?'
     assert condition.save
     
-    condition.comparison_mode = 'lt?'
-    assert condition.save
-    
-    condition.comparison_mode = 'gt?'
-    assert condition.save
-    
     condition.comparison_mode = 'ne?'
     assert condition.save
     
   end
   
   def test_evaluate_eql
-    condition = ProcessCondition.new(:process_field => @field, :field_value => 'value', :process_step => @step, :comparison_mode => 'eql?')
+    condition = ProcessCondition.new(:process_field => @field, :field_value => 'value', :step_if_true => @step, :comparison_mode => 'eql?')
     condition.save
     
     assert !condition.evaluate('another_value')
     assert condition.evaluate('value')
   end
   
-  def test_lt
-    condition = ProcessCondition.new(:process_field => @field, :field_value => '2', :process_step => @step, :comparison_mode => 'lt?')
+  def test_evaluate_ne
+    condition = ProcessCondition.new(:process_field => @field, :field_value => 'value', :step_if_true => @step, :comparison_mode => 'ne?')
     condition.save
     
-    assert !condition.evaluate('13')
-    assert !condition.evaluate('2')
-    assert condition.evaluate('1')
-    
+    assert !condition.evaluate('value')
+    assert condition.evaluate('!value')
   end
   
 end
