0602b03b0c0a208b0a9ebd30a55c7933ed3913e8
ande3577 <ande3577@gmail.com>
2013-08-08 13:52:11 -0500

    Fix warning when running test

--- test/unit/process_custom_field_test.rb
+++ test/unit/process_custom_field_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-class ProcessFieldTest < ActiveSupport::TestCase
+class ProcessCustomFieldTest < ActiveSupport::TestCase
   fixtures :trackers
   fixtures :issue_statuses
   fixtures :custom_fields
